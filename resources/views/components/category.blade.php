be52df788d7092d6092e3e318b9bbe7e25bb65e1
youssofOkiel <y.okiel@bimsaudi.com>
2022-09-05 18:54:11 +0200

    [added] category blade

--- resources/views/components/category.blade.php
+++ resources/views/components/category.blade.php
@@ -1,7 +1,9 @@
 <div>
-    <h5>
+   <a href="/categories/{{$category->slug}}">
+       <h5 class="{{ $category->depth == 0 ? 'font-bold': '' }}">
         {{$category->title}}
-    </h5>
+       </h5>
+   </a>
     @foreach($category->children as $child)
         <div class="ml-4">
             <x-category :category="$child"/>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,15 +16,13 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+                <div class="overflow-hidden grid md:grid-cols-2 lg:grid-cols-4">
                     @foreach($categories as $category)
 
                         <x-category :category="$category"/>
 
                     @endforeach
                 </div>
-            </div>
         </div>
     </div>
 </x-guest-layout>
